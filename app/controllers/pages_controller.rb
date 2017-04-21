83b27209db4b554e89d6032cc72319d1bb78e8f3
Jinnu Johnson <jinnudeveloper@gmail.com>
2017-04-21 12:24:59 +0000

    Added plans instance vars and query params to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
-    def home
-    end
-    def about
-    end
+  
+  def home
+    @basic_plan = Plan.find(1)
+    @pro_plan = Plan.find(2)
+  end
+  
+  def about
+  end
+  
 end
