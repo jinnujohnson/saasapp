4817835ae3faa339477d7986c00c50e012cb38eb
Jinnu Johnson <jinnudeveloper@gmail.com>
2017-04-27 06:04:44 +0000

    Added flash for selecting membership plans

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,6 +5,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
 # save with a special Stripe subscription function
 # Otherwise devise signup users with ordinary function
 
+  before_action :select_plan, only: :new
+  
   def create
     super do |resource|
       if params[:plan]
@@ -17,4 +19,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
       end
     end
   end
+  
+  private
+    def select_plan
+      unless (params[:plan] == '1' || params[:plan] == '2')
+        flash[:notice] = "Please select a membership plan to sign up."
+        redirect_to root_url
+      end
+    end
 end
