5c98f3327beecb3b2789c1a672c50b5b4112c5b7
Jinnu Johnson <jinnudeveloper@gmail.com>
2017-04-27 10:44:19 +0000

    Added profile and user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
          
   belongs_to :plan
+  has_one :user
   
   attr_accessor :stripe_card_token
   
