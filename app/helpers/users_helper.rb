ab72fa0b0a7ae8705bac72982e66623bd8ab5f7c
Jinnu Johnson <jinnudeveloper@gmail.com>
2017-05-14 06:05:18 +0000

    Home page UI improvements

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,7 +5,7 @@ module UsersHelper
       "<i class='fa fa-code'></i>".html_safe
     elsif @user.profile.job_title == "Entrepreneur"
       "<i class='fa fa-lightbulb-o'></i>".html_safe
-    elsif @user.profile.job_title == "Invester"
+    elsif @user.profile.job_title == "Investor"
       "<i class='fa fa-dollar'></i>".html_safe
     end
   end
